38e3b146c909e9800be6dbbfd888d76ea661bb5a
Bikaa2007 <69857458+Bikaa2007@users.noreply.github.com>
2022-07-12 19:33:35 +0530

    Add files via upload

---
+++ ngg.py
@@ -0,0 +1,21 @@
+import random
+print("Number Guessing Game")
+num=random.randint(1,9)
+chances=0
+print("Guess a number between 1-9")
+while chances<5:
+    guess=int(input("Enter your guess"))
+    if guess==num:
+        print("Congratulations YOU WON")
+        break
+
+    elif guess< num:
+        print("Your guess was too low: Guess a number higher than",guess)
+
+    else:
+        print("Your guess was too high: Guess a number lower than", guess)
+    chances+=1
+
+if not chances<5:
+    print("You lose!! The number is",num)
+
